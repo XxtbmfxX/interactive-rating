38276c011e64f422419a112e98223c71f1089c8d
Alexis Toro <aatn1321@gmail.com>
2022-07-27 12:01:32 -0400

    all readdy just readme needed

---
+++ index.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelector(".Main_rating").querySelectorAll("span");
+console.log(buttons);
+
+const mainCard = document.querySelector(".Main");
+const mainEnd = document.querySelector("#Main_end");
+const totalRate = document
+  .querySelector("#Main_end")
+  .querySelector(".Main_end-span");
+
+const button = document.querySelector(".Main_button");
+
+let rate = "";
+function manageButtons(e) {
+  rate = e.target.innerText;
+}
+
+function manageSubmi() {
+  mainCard.style.display = "none";
+  mainEnd.style.display = "flex";
+  totalRate.innerHTML = `You selected ${rate} out of 5`;
+}
+
+buttons.forEach((button) => button.addEventListener("click", manageButtons));
+button.addEventListener("click", manageSubmi);
